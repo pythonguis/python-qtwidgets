a0ff8c0f6bbc803ac3a393d19343497cdaafbec0
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2020-01-31 21:16:18 +0100

    Move to package structure.

---
+++ qtwidgets/__init__.py
@@ -0,0 +1,12 @@
+from colorbutton import ColorButton
+# from color_duo
+# from equalizer
+from equalizer_bar import Equalizer
+# from filebrowser
+from gradient import Gradient
+from paint import Paint
+from passwordedit import PasswordEdit
+from power_bar import PowerBar
+from palette import Palette
+# from scrubber
+# from stopwatch
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(
+    name='qtwidgets',
+    version=version,
+    url='http://github.com/learnpyqt/qtwidgets,
+    author='Martin Fitzpatrick',
+    author_email='martin.fitzpatrick@gmail.com',
+    description='Custom widget library for PyQt5 and PySide2 (Qt for Python). Free to use in your own applications.',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Desktop Environment',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Widget Sets',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4'
+    ]
+)
