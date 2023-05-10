a36a287a0178e5ccebb6c6c279befac5c652fba8
girvile <girvile1@gmail.com>
2023-05-10 12:18:03 +0200

    Update colorbutton.py
    Fix module for pyQt6

--- qtwidgets/colorbutton/colorbutton.py
+++ qtwidgets/colorbutton/colorbutton.py
@@ -1,15 +1,10 @@
 import sys
-if 'PyQt5' in sys.modules:
-    from PyQt5 import QtCore, QtGui, QtWidgets
-    from PyQt5.QtCore import Qt, pyqtSignal as Signal
-
-else:
-    from PySide2 import QtCore, QtGui, QtWidgets
-    from PySide2.QtCore import Qt, Signal
+from qtpy.QtCore import Signal
+from qtpy.QtWidgets import QPushButton
 
 
 
-class ColorButton(QtWidgets.QPushButton):
+class ColorButton(QPushButton):
     '''
     Custom Qt Widget to show a chosen color.
 
